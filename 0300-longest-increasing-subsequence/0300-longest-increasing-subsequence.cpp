5baea90186c65ba520a1a70c4902a8143fb5b7e7
Areeb-Ansari <94287249+Areeb-Ansari@users.noreply.github.com>
2022-10-24 23:36:15 +0530

    Time: 22 ms (76.99%), Space: 10.5 MB (71.39%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -69,6 +69,26 @@ private:
         return currRow[0];
     }
     
+    int solveOptimal(vector<int>& nums){
+        int n = nums.size();
+        if(n==0)
+            return 0;
+        
+        vector<int>ans;
+        ans.push_back(nums[0]);
+        
+        for(int i=1; i<n; i++){
+            if(nums[i] > ans.back()){
+                ans.push_back(nums[i]);
+            }
+            else{
+                int index = lower_bound(ans.begin(),ans.end(),nums[i]) - ans.begin();
+                ans[index] = nums[i];
+            }
+        }
+        return ans.size();
+    }
+    
 public:
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
@@ -79,6 +99,8 @@ public:
         
         //return solveTab(nums);
         
-        return solveSpace(nums);
+        // return solveSpace(nums);
+        
+        return solveOptimal(nums); // with binary search
     }
 };
